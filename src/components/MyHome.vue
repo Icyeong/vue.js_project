1eaf5b6b0d9f740c00381a645374de1705da1268
임채영(cyim) <dbcjswjs@gmail.com>
2023-03-13 15:45:54 +0900

    setting

---
+++ src/components/MyHome.vue
@@ -0,0 +1,57 @@
+<script src="https://unpkg.com/vue@3"></script>
+<script src="https://unpkg.com/vue-router@4"></script>
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      vue js project 생성 완료
+    </p>
+    <h2>router 설정</h2>
+    <h2>Home page</h2>
+
+    <ul>
+      <li><a href="/about" rel="noopener">Go to About</a></li>
+    </ul>
+  
+      <router-link to="/about">Go to About</router-link>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+     <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHome',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+h1{
+  color: green;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
